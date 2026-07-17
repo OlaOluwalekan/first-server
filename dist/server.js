9580b4186755c4df8b46798ffaa7b87a6bcff25c
OlaOluwalekan <olalekanbello534@gmail.com>
2026-07-17 16:22:52 +0100

    feat: initialize Express server with EJS templating and API documentation support

--- dist/server.js
+++ dist/server.js
@@ -6,13 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const todo_route_1 = __importDefault(require("./routes/todo.route"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const todos_json_1 = __importDefault(require("./docs/todos.json"));
 const app = (0, express_1.default)();
+// EJS view engine setup
+app.set('view engine', 'ejs');
+app.set('views', path_1.default.join(__dirname, '../src/templates'));
 app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Hello World! This is a First Server!!! You think 😁😁😁. Now working?');
+    res.render('index');
 });
 app.use('/api/docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(todos_json_1.default));
 app.use('/api/todos', todo_route_1.default);
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import path from 'path'
 import todoRoutes from './routes/todo.route'
 import swaggerUI from 'swagger-ui-express'
 import todoDocs from './docs/todos.json'
 
 const app = express()
 
+// EJS view engine setup
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '../src/templates'))
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send(
-    'Hello World! This is a First Server!!! You think 😁😁😁. Now working?',
-  )
+  res.render('index')
 })
 
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(todoDocs))
